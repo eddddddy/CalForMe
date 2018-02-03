d94501a68a1eca0a0f70f9756341c12f56d3ad44
eddddddy <eddy3.14159@gmail.com>
2018-02-03 17:06:50 -0500

    update

--- app/src/main/java/com/example/edward/calforme/MainActivity.java
+++ app/src/main/java/com/example/edward/calforme/MainActivity.java
@@ -17,13 +17,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.graphics.BitmapFactory;
 import android.view.View;
 
-import java.io.ByteArrayOutputStream
-
-import org.json.*;
-
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.s3.transferutility.*;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
     String mCurrentPhotoPath;
     String url;
     FragmentTransaction transaction;
+    private final String key = "AKIAIPS5WU5KQZXC5T6Q";
+    private final String secret = "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";
+    private final String bucket = "arn:aws:s3:::calforme-deployments-mobilehub-129629321";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         AWSMobileClient.getInstance().initialize(this).execute();
 
         imageView = findViewById(R.id.imageView);
-        url = "https://nickc158.lib.id/caloriecounter@dev";
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         File photoFile = null;
@@ -85,15 +91,61 @@ public class MainActivity extends AppCompatActivity {
             }).start();
             */
 
-            new Thread(new Runnable() {
-                public void run() {
-                    sendToClarifai(myBitmap);
-                }
-            }).start();
+
+            uploadData();
 
         }
     }
 
+
+    public void uploadData() {
+
+        // Initialize AWSMobileClient if not initialized upon the app startup.
+        // AWSMobileClient.getInstance().initialize(this).execute();
+
+        TransferUtility transferUtility =
+                TransferUtility.builder()
+                        .context(getApplicationContext())
+                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .build();
+
+        File f = new File(mCurrentPhotoPath);
+        System.out.println(f);
+
+        TransferObserver uploadObserver = transferUtility.upload("test.jpg", f);
+
+        uploadObserver.setTransferListener(new TransferListener() {
+
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (TransferState.COMPLETED == state) {
+                    // Handle a completed upload.
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+                int percentDone = (int) percentDonef;
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                ex.printStackTrace();
+            }
+
+        });
+
+        // If your upload does not trigger the onStateChanged method inside your
+        // TransferListener, you can directly check the transfer state as shown here.
+        if (TransferState.COMPLETED == uploadObserver.getState()) {
+            // Handle a completed upload.
+        }
+    }
+
+
+    /*
     public byte[] getBytesFromBitmap(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
@@ -136,6 +188,8 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(stringRequest.toString());
         queue.add(stringRequest);
     }
+*/
+
 
     public void onRedoClick(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
